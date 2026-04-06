72bbcf7df01e50e8a7a1e7283e5c0c765495aeb4
phpdot <git@phpdot.com>
2026-04-06 14:53:04 +0300

    Add Config and Binds attributes
    Config: marks a readonly class as a config DTO, maps to config file by name.
    Binds: marks a class as the default implementation for an interface. Repeatable.

---
+++ src/Attribute/Binds.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPdot\Container\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
+final class Binds
+{
+    /**
+     * @param class-string $interface The interface this class is the default for
+     */
+    public function __construct(
+        public readonly string $interface,
+    ) {
+    }
+}
---
+++ src/Attribute/Config.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPdot\Container\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+final class Config
+{
+    /**
+     * @param string $name Config file name (without .php extension)
+     */
+    public function __construct(
+        public readonly string $name,
+    ) {
+    }
+}
